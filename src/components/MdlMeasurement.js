183d498102bf5f26c55bb9c0c296fee05275a24a
egiit <giefilth@gmail.com>
2023-07-15 10:59:22 +0700

    update measurement modal

--- src/components/MdlMeasurement.js
+++ src/components/MdlMeasurement.js
@@ -23,14 +23,40 @@ const MdlMeasurement = ({ handleClose }) => {
   const [dataTemp, setDataTemp] = useState([]);
   const [dataPostMes, setDataPostMes] = useState([]);
   const [opsiSeq, setOpsiSeq] = useState(false);
+  const [modalInput, setMdalInput] = useState(false);
   const inputShow = useRef(null);
+  const listBtn = {
+    INCH: ["1/16", "1/8", "1/4", "1/2", "1/3", "3/16", "3/8", "3/4"],
+    CM: [
+      "0.1",
+      "0.2",
+      "0.3",
+      "0.4",
+      "0.5",
+      "0.6",
+      "0.7",
+      "0.8",
+      "0.9",
+      "1.00",
+      "1.01",
+      "1.02",
+      "1.03",
+      "1.04",
+      "1.05",
+      "1.06",
+      "1.07",
+      "1.08",
+      "1.09",
+    ],
+  };
 
   function activeInput() {
     if (seq === 6) return flash(`Sudah 5 urutan `, 2500, "warning");
-    const mdl = document.getElementById("mdlInpt");
-    const cntn = document.getElementById("contentMdl");
-    mdl.classList.toggle("show");
-    cntn.classList.toggle("show");
+    // const mdl = document.getElementById("mdlInpt");
+    // const cntn = document.getElementById("contentMdl");
+    // mdl.classList.toggle("show");
+    // cntn.classList.toggle("show");
+    setMdalInput(modalInput ? false : true);
   }
 
   //buton prev
@@ -46,8 +72,10 @@ const MdlMeasurement = ({ handleClose }) => {
         }
       }
       setIdxInput(newIdx);
+      checkIdx(newIdx);
     } else {
       setIdxInput(0);
+      checkIdx(0);
     }
   }
 
@@ -64,8 +92,10 @@ const MdlMeasurement = ({ handleClose }) => {
         }
       }
       setIdxInput(newIdx);
+      checkIdx(newIdx);
     } else {
       setIdxInput(state.measurSpec.length - 1);
+      checkIdx(state.measurSpec.length - 1);
     }
   }
 
@@ -78,7 +108,7 @@ const MdlMeasurement = ({ handleClose }) => {
 
   function handleChange(e, idx, spec) {
     e.preventDefault();
-    const regex = /^[0-9\s/]{0,6}$/;
+    const regex = /^[0-9\s/.]{0,6}$/;
 
     const { value } = e.target;
     if (spec.MES_UOM === "INCH" && !regex.test(value)) {
@@ -193,11 +223,29 @@ const MdlMeasurement = ({ handleClose }) => {
     return false;
   }
 
-  useEffect(() => {
-    if (dataTemp.length > 0 && dataTemp[idxInput]) {
+  // useEffect(() => {
+  //   if (modalInput) {
+  //     if (dataTemp.length > 0 && dataTemp[idxInput]) {
+  //       inputShow.current.focus();
+  //       if (dataTemp[idxInput].MES_VALUE.toString() === "0") {
+  //         inputShow.current.value = dataTemp[idxInput].MES_VALUE;
+  //         passCheckVal(true);
+  //       } else {
+  //         inputShow.current.value = "";
+  //         passCheckVal(false);
+  //       }
+  //     } else {
+  //       inputShow.current.value = "";
+  //       passCheckVal(false);
+  //     }
+  //   }
+  // }, [idxInput, dataTemp, modalInput]);
+
+  function checkIdx(idx) {
+    if (dataTemp.length > 0 && dataTemp[idx]) {
       inputShow.current.focus();
-      if (dataTemp[idxInput].MES_VALUE.toString() === "0") {
-        inputShow.current.value = dataTemp[idxInput].MES_VALUE;
+      if (dataTemp[idx].MES_VALUE.toString() === "0") {
+        inputShow.current.value = dataTemp[idx].MES_VALUE;
         passCheckVal(true);
       } else {
         inputShow.current.value = "";
@@ -207,7 +255,7 @@ const MdlMeasurement = ({ handleClose }) => {
       inputShow.current.value = "";
       passCheckVal(false);
     }
-  }, [idxInput, dataTemp]);
+  }
 
   async function getDataMeasOut(dataBundleSel, siteName, lineName) {
     const { BARCODE_SERIAL, ORDER_SIZE } = dataBundleSel;
@@ -277,10 +325,7 @@ const MdlMeasurement = ({ handleClose }) => {
 
     setSeq(sq);
     setDataTemp(checkDataPost);
-    const mdl = document.getElementById("mdlInpt");
-    const cntn = document.getElementById("contentMdl");
-    mdl.classList.toggle("show");
-    cntn.classList.toggle("show");
+    setMdalInput(true);
   }
 
   function cancelOpsi() {
@@ -320,6 +365,13 @@ const MdlMeasurement = ({ handleClose }) => {
       .catch((err) => flash(err.message, 2500, "danger"));
   }
 
+  let uom = state.measurSpec[0].MES_UOM;
+
+  function selectButon(value) {
+    const spec = state.measurSpec[idxInput];
+    handleInptMes(value, idxInput, spec);
+  }
+
   return (
     <Modal
       //   size="xl"
@@ -329,176 +381,191 @@ const MdlMeasurement = ({ handleClose }) => {
       className="mainmodal"
     >
       <Modal.Body>
-        <div style={{ height: "90vh" }}>
-          <Row className="mb-2">
-            <Col>
-              <div className="bg-ylw  shadow-sm py-1">
-                <h4 className="text-center mb-0">MEASUREMENT</h4>
-              </div>
-            </Col>
-          </Row>
-          <Row>
-            <Col sm={2}>
-              <InputGroup size="sm" className="mb-3">
-                <InputGroup.Text id="no-box" className="fw-bold">
-                  NO BOX
-                </InputGroup.Text>
-                <Form.Control
-                  placeholder="nobox"
-                  aria-label="nobox"
-                  aria-describedby="no-box"
-                  disabled
-                  defaultValue={state.qrMeasSelected?.BUNDLE_SEQUENCE}
-                />
-              </InputGroup>
-            </Col>
-            <Col sm={3}>
-              <InputGroup size="sm" className="mb-3">
-                <InputGroup.Text id="qr" className="fw-bold">
-                  QR SERIAL
-                </InputGroup.Text>
-                <Form.Control
-                  placeholder="qr"
-                  aria-label="qr"
-                  aria-describedby="qr"
-                  disabled
-                  defaultValue={state.qrMeasSelected?.BARCODE_SERIAL}
-                />
-              </InputGroup>
-            </Col>
-            <Col sm={2}>
-              <InputGroup size="sm" className="mb-3">
-                <InputGroup.Text id="sizes" className="fw-bold">
-                  SIZE
-                </InputGroup.Text>
-                <Form.Control
-                  placeholder="sizes"
-                  aria-label="sizes"
-                  aria-describedby="sizes"
-                  disabled
-                  defaultValue={state.qrMeasSelected?.ORDER_SIZE}
-                />
-              </InputGroup>
-            </Col>
-            <Col sm={3} className="ms-auto text-end">
-              <Button
-                size="sm"
-                variant="primary"
-                onClick={() => activeInput()}
-                disabled={state.measurSpec?.length === 0 || seq === 6}
-              >
-                ADD : #{seq > 5 ? 5 : seq}
-              </Button>
-            </Col>
-          </Row>
-          {opsiSeq ? (
-            <Row className="justify-content-end mb-2">
-              <Col sm={3} className="text-end fw-bold">
-                Acion for Sequance {opsiSeq} :
+        {!modalInput ? (
+          <div style={{ height: "90vh" }}>
+            <Row className="mb-2">
+              <Col>
+                <div className="bg-ylw  shadow-sm py-1">
+                  <h4 className="text-center mb-0">MEASUREMENT</h4>
+                </div>
               </Col>
-              <Col sm={3} className="text-end">
+            </Row>
+            <Row>
+              <Col sm={2}>
+                <InputGroup size="sm" className="mb-3">
+                  <InputGroup.Text id="no-box" className="fw-bold">
+                    NO BOX
+                  </InputGroup.Text>
+                  <Form.Control
+                    placeholder="nobox"
+                    aria-label="nobox"
+                    aria-describedby="no-box"
+                    disabled
+                    defaultValue={state.qrMeasSelected?.BUNDLE_SEQUENCE}
+                  />
+                </InputGroup>
+              </Col>
+              <Col sm={3}>
+                <InputGroup size="sm" className="mb-3">
+                  <InputGroup.Text id="qr" className="fw-bold">
+                    QR SERIAL
+                  </InputGroup.Text>
+                  <Form.Control
+                    placeholder="qr"
+                    aria-label="qr"
+                    aria-describedby="qr"
+                    disabled
+                    defaultValue={state.qrMeasSelected?.BARCODE_SERIAL}
+                  />
+                </InputGroup>
+              </Col>
+              <Col sm={2}>
+                <InputGroup size="sm" className="mb-3">
+                  <InputGroup.Text id="sizes" className="fw-bold">
+                    SIZE
+                  </InputGroup.Text>
+                  <Form.Control
+                    placeholder="sizes"
+                    aria-label="sizes"
+                    aria-describedby="sizes"
+                    disabled
+                    defaultValue={state.qrMeasSelected?.ORDER_SIZE}
+                  />
+                </InputGroup>
+              </Col>
+              <Col sm={3} className="ms-auto text-end">
                 <Button
                   size="sm"
-                  variant="outline-danger"
-                  className="me-3"
-                  onClick={() => deleteSeqData(opsiSeq)}
+                  variant="primary"
+                  onClick={() => activeInput()}
+                  disabled={state.measurSpec?.length === 0 || seq === 6}
                 >
-                  Delete <IoMdRemoveCircleOutline size={15} />
-                </Button>
-                <Button
-                  size="sm"
-                  variant="outline-warning"
-                  className="me-3"
-                  onClick={() => editSeq(opsiSeq)}
-                >
-                  Edit <IoMdCreate size={15} />
-                </Button>
-                <Button
-                  size="sm"
-                  variant="outline-secondary"
-                  className="me-3"
-                  onClick={() => cancelOpsi()}
-                >
-                  Cancel
+                  ADD : #{seq > 5 ? 5 : seq}
                 </Button>
               </Col>
             </Row>
-          ) : null}
-          <Row className="mb-3">
-            <Col>
-              <div className="table-responsive" style={{ height: "83vh" }}>
-                <Table size="sm" bordered hover>
-                  <thead>
-                    <tr
-                      className="text-center"
-                      style={{ position: "sticky", top: 0, zIndex: 1 }}
-                    >
-                      <th className="bg-secondary text-light">NO</th>
-                      <th className="bg-secondary text-light">DESCRIPTION</th>
-                      <th className="bg-secondary text-light">SPEC</th>
-                      <th className="bg-secondary text-light">TOL-</th>
-                      <th className="bg-secondary text-light">TOL+</th>
-                      <th className="bg-seq" onClick={() => seqOpsAction(1)}>
-                        1
-                      </th>
-                      <th className="bg-seq" onClick={() => seqOpsAction(2)}>
-                        2
-                      </th>
-                      <th className="bg-seq" onClick={() => seqOpsAction(3)}>
-                        3
-                      </th>
-                      <th className="bg-seq" onClick={() => seqOpsAction(4)}>
-                        4
-                      </th>
-                      <th className="bg-seq" onClick={() => seqOpsAction(5)}>
-                        5
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {state.measurSpec.length ? (
-                      state.measurSpec?.map((spec, i) => (
-                        <tr key={i} className="text-center">
-                          <td>{i + 1}</td>
-                          <td className="text-start">{spec.POM_DESC}</td>
-                          <td>{spec.SPEC}</td>
-                          <td>{spec.POM_PLUS}</td>
-                          <td>{spec.POM_MIN}</td>
-                          <td>{findOutputMes(dataPostMes, spec.POM_ID, 1)}</td>
-                          <td>{findOutputMes(dataPostMes, spec.POM_ID, 2)}</td>
-                          <td>{findOutputMes(dataPostMes, spec.POM_ID, 3)}</td>
-                          <td>{findOutputMes(dataPostMes, spec.POM_ID, 4)}</td>
-                          <td>{findOutputMes(dataPostMes, spec.POM_ID, 5)}</td>
-                        </tr>
-                      ))
-                    ) : (
-                      <tr>
-                        <td className="fst-italic text-center" colSpan={10}>
-                          No Data SPEC Please Call QC ADM
-                        </td>
+            {opsiSeq ? (
+              <Row className="justify-content-end mb-2">
+                <Col sm={3} className="text-end fw-bold">
+                  Acion for Sequance {opsiSeq} :
+                </Col>
+                <Col sm={3} className="text-end">
+                  <Button
+                    size="sm"
+                    variant="outline-danger"
+                    className="me-3"
+                    onClick={() => deleteSeqData(opsiSeq)}
+                  >
+                    Delete <IoMdRemoveCircleOutline size={15} />
+                  </Button>
+                  <Button
+                    size="sm"
+                    variant="outline-warning"
+                    className="me-3"
+                    onClick={() => editSeq(opsiSeq)}
+                  >
+                    Edit <IoMdCreate size={15} />
+                  </Button>
+                  <Button
+                    size="sm"
+                    variant="outline-secondary"
+                    className="me-3"
+                    onClick={() => cancelOpsi()}
+                  >
+                    Cancel
+                  </Button>
+                </Col>
+              </Row>
+            ) : null}
+            <Row className="mb-3">
+              <Col>
+                <div className="table-responsive" style={{ height: "83vh" }}>
+                  <Table size="sm" bordered hover>
+                    <thead>
+                      <tr
+                        className="text-center"
+                        style={{ position: "sticky", top: 0, zIndex: 1 }}
+                      >
+                        <th className="bg-secondary text-light">NO</th>
+                        <th className="bg-secondary text-light">DESCRIPTION</th>
+                        <th className="bg-secondary text-light">SPEC</th>
+                        <th className="bg-secondary text-light">TOL-</th>
+                        <th className="bg-secondary text-light">TOL+</th>
+                        <th className="bg-seq" onClick={() => seqOpsAction(1)}>
+                          1
+                        </th>
+                        <th className="bg-seq" onClick={() => seqOpsAction(2)}>
+                          2
+                        </th>
+                        <th className="bg-seq" onClick={() => seqOpsAction(3)}>
+                          3
+                        </th>
+                        <th className="bg-seq" onClick={() => seqOpsAction(4)}>
+                          4
+                        </th>
+                        <th className="bg-seq" onClick={() => seqOpsAction(5)}>
+                          5
+                        </th>
                       </tr>
-                    )}
-                  </tbody>
-                </Table>
-              </div>
-            </Col>
-          </Row>
-          <Row className="justify-content-end">
-            <Col sm={1} className="text-end">
-              <Button size="sm" variant="primary" onClick={() => handleClose()}>
-                Close
-              </Button>
-            </Col>
-          </Row>
-        </div>
-        <div id="mdlInpt" className="bg-maeasur-inpt">
-          <div id="contentMdl" className="mdl-input-measure">
+                    </thead>
+                    <tbody>
+                      {state.measurSpec.length ? (
+                        state.measurSpec?.map((spec, i) => (
+                          <tr key={i} className="text-center">
+                            <td>{i + 1}</td>
+                            <td className="text-start">{spec.POM_DESC}</td>
+                            <td>{spec.SPEC}</td>
+                            <td>{spec.POM_PLUS}</td>
+                            <td>{spec.POM_MIN}</td>
+                            <td>
+                              {findOutputMes(dataPostMes, spec.POM_ID, 1)}
+                            </td>
+                            <td>
+                              {findOutputMes(dataPostMes, spec.POM_ID, 2)}
+                            </td>
+                            <td>
+                              {findOutputMes(dataPostMes, spec.POM_ID, 3)}
+                            </td>
+                            <td>
+                              {findOutputMes(dataPostMes, spec.POM_ID, 4)}
+                            </td>
+                            <td>
+                              {findOutputMes(dataPostMes, spec.POM_ID, 5)}
+                            </td>
+                          </tr>
+                        ))
+                      ) : (
+                        <tr>
+                          <td className="fst-italic text-center" colSpan={10}>
+                            No Data SPEC Please Call QC ADM
+                          </td>
+                        </tr>
+                      )}
+                    </tbody>
+                  </Table>
+                </div>
+              </Col>
+            </Row>
+            <Row className="justify-content-end">
+              <Col sm={1} className="text-end">
+                <Button
+                  size="sm"
+                  variant="primary"
+                  onClick={() => handleClose()}
+                >
+                  Close
+                </Button>
+              </Col>
+            </Row>
+          </div>
+        ) : (
+          <div>
             <Row className="justify-content-center">
               <Col sm={1} className="text-center bg-primary rounded py-1">
                 <h4 className="text-light mb-0">#{seq}</h4>
               </Col>
             </Row>
-            <Row style={{ height: "18vh" }} className="pt-1">
+            <Row className="pt-1">
               <Col sm={1}>
                 <Button
                   className="mt-4"
@@ -588,7 +655,25 @@ const MdlMeasurement = ({ handleClose }) => {
                 </Button>
               </Col>
             </Row>
-            <Row className="justify-content-end mt-3 pe-5">
+            <Row>
+              <Col className="mx-4 mt-2">
+                <div className="text-center fw-bold bg-dark bg-opacity-25 mb-1">
+                  Tombol Pintas {uom}
+                </div>
+                {listBtn[uom].map((lbtn, i) => (
+                  <Button
+                    key={i}
+                    size="sm"
+                    variant="primary"
+                    onClick={() => selectButon(lbtn)}
+                    className="m-2"
+                  >
+                    {lbtn}
+                  </Button>
+                ))}
+              </Col>
+            </Row>
+            <Row className="justify-content-end mt-3 pe-5 border-2 border-top pt-2">
               <Col sm={3} className="text-end ">
                 <Button
                   className="me-2"
@@ -609,7 +694,7 @@ const MdlMeasurement = ({ handleClose }) => {
               </Col>
             </Row>
           </div>
-        </div>
+        )}
       </Modal.Body>
     </Modal>
   );
